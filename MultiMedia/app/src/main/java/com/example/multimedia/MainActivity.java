6ba1a59a3b8a3c60b96cc23e7d4a5558603cea8e
Jian.Cui <cuijianjoy@126.com>
2020-03-24 00:42:13 +0800

    open album
    Signed-off-by: Jian.Cui <cuijianjoy@126.com>

---
+++ MultiMedia/app/src/main/java/com/example/multimedia/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.multimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cameraAlbumButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraAlbumButton = findViewById(R.id.camera_album_button);
+        cameraAlbumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.multimedia.CameraAlbum");
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+}
